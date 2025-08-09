082a0a347fc6daa137e79d7dda58010a9d5a8a3e
Claudio Yáñez <epicrealms@gmail.com>
2025-08-08 23:29:34 -0400

    Start developing users module

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+describe('UsersController', () => {
+  let usersController: UsersController;
+  const mockUsersService: jest.Mocked<UsersService> = {
+    findAll: jest.fn<string[], []>(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [{ provide: UsersService, useValue: mockUsersService }],
+    }).compile();
+
+    usersController = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(usersController).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should get all users', () => {
+      // ARRANGE
+      const expectedUsers = ['user1', 'user2'];
+      mockUsersService.findAll.mockReturnValue(expectedUsers);
+      // ACT
+      const result = usersController.findAll(); // GET <urlBase>/users/
+      // ASSERT
+      expect(result).toEqual(expectedUsers);
+      expect(mockUsersService.findAll).toHaveBeenCalled();
+    });
+    it.todo('should fail to get a user if it does not exist');
+  });
+});
---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return ['user1', 'user2'];
+    // return usersService.findAll();
+  }
+}
