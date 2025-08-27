c2ec1f65b5053cae4e80240bd62b556b290dd11a
aclementcl <alejandro.clemente.i@gmail.com>
2025-08-27 15:55:33 -0400

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Config Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Skills Manager API')
+    .setDescription('API para gestionar Skills y Users')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
