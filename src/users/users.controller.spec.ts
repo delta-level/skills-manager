2a9d4bd81fe8a4c3837ab2097f50312daca9dbde
Claudio Yanez <claudio.yanez@ug.uchile.cl>
2025-10-14 20:44:26 -0300

    Implements users.controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { UserDocument } from './schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
+import { NotFoundException } from '@nestjs/common';
+import { Types } from 'mongoose';
 
 describe('UsersController', () => {
   let usersController: UsersController;
   const mockUsersService = {
+    create: jest.fn(),
     findAll: jest.fn(),
     findById: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
   } as unknown as jest.Mocked<UsersService>;
 
   beforeEach(async () => {
@@ -27,7 +32,21 @@ describe('UsersController', () => {
     expect(usersController).toBeDefined();
   });
 
-  describe('getAll', () => {
+  describe('create', () => {
+    it('should create a user', async () => {
+      // ARRANGE
+      const user: User = { email: 'email', name: 'name' };
+      const createdUser = { ...user, _id: 'userId' };
+      mockUsersService.create.mockResolvedValue(createdUser as UserDocument);
+      // ACT
+      const result = await usersController.create(user);
+      // ASSERT
+      expect(result).toEqual(createdUser);
+      expect(mockUsersService.create).toHaveBeenCalledWith(user);
+    });
+  });
+
+  describe('findAll', () => {
     it('should get all users', async () => {
       // ARRANGE
       const expectedUsers = ['user1', 'user2'];
@@ -35,11 +54,94 @@ describe('UsersController', () => {
         expectedUsers as unknown as UserDocument[],
       );
       // ACT
-      const result = await usersController.findAll(); // GET <urlBase>/users/
+      const result = await usersController.findAll();
       // ASSERT
       expect(result).toEqual(expectedUsers);
       expect(mockUsersService.findAll).toHaveBeenCalled();
     });
-    it.todo('should fail to get a user if it does not exist');
+  });
+
+  describe('findById', () => {
+    it('should find user by id', async () => {
+      // ARRANGE
+      const user = 'user';
+      mockUsersService.findById.mockResolvedValue(
+        user as unknown as UserDocument,
+      );
+      // ACT
+      const result = await usersController.findById('id');
+      // ASSERT
+      expect(result).toBe(user);
+      expect(mockUsersService.findById).toHaveBeenCalledWith('id');
+    });
+    it('should throw error 404 if no user is found', async () => {
+      // ARRANGE
+      mockUsersService.findById.mockRejectedValue(
+        new NotFoundException('User with id "id" not found'),
+      );
+      // ACT & ASSERT
+      await expect(usersController.findById('id')).rejects.toThrow(
+        new NotFoundException('User with id "id" not found'),
+      );
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user by id', async () => {
+      // ARRANGE
+      const userId = 'userId';
+      const updateData = { email: 'newEmail' };
+      const updatedUser: Partial<UserDocument> = {
+        _id: userId as unknown as Types.ObjectId,
+        email: 'newEmail',
+        name: 'name',
+      };
+      mockUsersService.update.mockResolvedValue(updatedUser as UserDocument);
+      // ACT
+      const result = await usersController.update(userId, updateData);
+      // ASSERT
+      expect(result).toEqual(updatedUser);
+      expect(mockUsersService.update).toHaveBeenCalledWith(userId, updateData);
+    });
+    it('should throw error 404 if no user is found', async () => {
+      // ARRANGE
+      mockUsersService.update.mockRejectedValue(
+        new NotFoundException('User with id "userId" not found'),
+      );
+      // ACT & ASSERT
+      await expect(
+        usersController.update('userId', { email: 'newEmail' }),
+      ).rejects.toThrow(
+        new NotFoundException('User with id "userId" not found'),
+      );
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete a user by id', async () => {
+      // ARRANGE
+      const userId = 'userId';
+      const deletedUser: Partial<UserDocument> = {
+        _id: userId,
+        email: 'email',
+        name: 'name',
+      };
+      mockUsersService.delete.mockResolvedValue(deletedUser as UserDocument);
+      // ACT
+      const result = await usersController.delete(userId);
+      // ASSERT
+      expect(result).toEqual(deletedUser);
+      expect(mockUsersService.delete).toHaveBeenCalledWith(userId);
+    });
+    it('should throw error 404 if no user is found', async () => {
+      // ARRANGE
+      mockUsersService.delete.mockRejectedValue(
+        new NotFoundException('User with id "userId" not found'),
+      );
+      // ACT & ASSERT
+      await expect(usersController.delete('userId')).rejects.toThrow(
+        new NotFoundException('User with id "userId" not found'),
+      );
+    });
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './schemas/user.schema';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Post()
+  create(@Body() user: User) {
+    return this.usersService.create(user);
+  }
+
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return this.usersService.findById(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateData: Partial<User>) {
+    return this.usersService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.usersService.delete(id);
   }
 }
