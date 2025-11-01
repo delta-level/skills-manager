7842a5f925a26df28d98ed370dc226ed1e2d76bf
Claudio Yanez M. <claudio.yanez@ug.uchile.cl>
2025-11-01 12:08:10 -0300

    Adds swagger documentation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,19 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Nombre del usuario',
+    example: 'Juan Pérez',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'Correo electrónico del usuario',
+    example: 'juan.perez@example.com',
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
